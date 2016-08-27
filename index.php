bccb0c765baa58c19d80cf59a3c86ed875317bc1
Evelina Cretu <evelina.cretu@gmail.com>
2016-08-27 23:40:03 +0300

    Minor changes with phone

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 if($_POST["submit"]) {
     $recipient="evelina.cretu@gmail.com";
     $subject="Rezervarea biletului Chisinau-Bucuresti";
@@ -7,21 +6,17 @@ if($_POST["submit"]) {
     $email=$_POST["email"];
     $telefon=$_POST["telefon"];
     $data=$_POST["data"];
-
     $mailBody="Name: $firstname\nEmail: $email\nTelefon: $telefon\nData: $data\n\n";
-
     mail($recipient, $subject, $mailBody, "From: $firstname <$email>");
-
     $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
-
 ?><!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta http-equiv="Cache-Control" content="no-cache">
 	<meta name="description" content="Transport spre Bucuresti traseu pornește din Chișinău. Zilnic la 18:00. Pe traseu Barlad, Focsani, Buzau, Ploiesti si Aeroportul Otopeni. Automobil comfortabil, transportare sigură și rapidă. Acceptam solicitări la comandă. Transportăm colete, documente, pasageri, automobile la tral.">
-	<meta name="keywords" content="transport, europa, pasageri, colete, Bucuresti, Chisinau, Moldova, rutiera, md">
+	<meta name="keywords" content="transport, europa, pasageri, colete, Bucuresti, Chisinau, Moldova, rutiera, md, cetatenie, Romania, documente, 8, dimineata, +40755380914">
 	<meta name="author" content="Rutiera Chisinau Bucuresti">
 	<title>Transport Chisinau Bucuresti</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -32,11 +27,11 @@ if($_POST["submit"]) {
 	<section class="toppart">
 	<div class="info" alt="traseul, ruta, drumul, orase">
 			<h1>CHIȘINĂU<br>BUCUREȘTI</h1>
-			<h2>Zilnic la 18:00</h2>
+			<h2>Zilnic la 08:00 și 18:00</h2>
 			<div class="container">
 				<div class="phone_header prime"><a href="callto://+37376004389">+373 76004389</a></div>
-				<div class="phonering_header prime"><a href="callto://+37376004389" style="padding-right: 0px !important;">+373 76004389</a></div>
-				<h3>Bârlad, Focșani, Buzău, Ploiești<br>și Aeroportul Otopeni</h3>
+				<div class="phonering_header prime"><a href="callto://+37376004389" style="padding-right: 0px !important;">+373 +40755380914</a></div>
+				<h3>Transport la depunerea actelor<br>pentru cetățenie română</h3>
 		</div>
 	</section>
 
@@ -47,7 +42,7 @@ if($_POST["submit"]) {
 						<img class="gif" src="img/transport.png"/>
 					</div>
 				<h3 class="h3er">Transport la comandă în Europa</h3>
-				<p class="pinfo">Organizăm transport zilnic la București la ora 18:00. La fel este posibila arendarea rutei spre alte destinatii in Romania.</p>
+				<p class="pinfo">Organizăm transport zilnic la București la ora 08:00 și 18:00. La fel este posibila arendarea rutei spre alte destinatii in Romania.</p>
 			</div>
 			<div class="colete">
 					<div class="gif_container">
@@ -86,7 +81,7 @@ if($_POST["submit"]) {
 					<div>
 						<a>+373 76004389 &nbsp; +373 69834643</a>
 						<br>
-						<a>+40 756991566 &nbsp; +40 746763695</a>
+						<a>+40 756991566 &nbsp; +40755380914 &nbsp; +40755597619</a>
 					</div>
 				</li>
 				<li class="ponering white"><a href="">+373 76004389</a></li>
